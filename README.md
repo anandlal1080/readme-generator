48c6e8090c142b1aabd85a9d3974c685fe75d474
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2020-11-25 09:37:36 -0700

    Working Readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# readme-generator
+## Title
+
+lakdfjgmvljmfgv
+
+## Description
+
+a'ljsdmgvlkjfmgv
+
+## Installation
+
+```npm install password-generator```
+
+## Usage
+
+almksjgmfcafg
+
+## Contribution
+
+askfgjhnarfg
+
+## Project Testing
+
+asekhcnhg
+
+## License
+
+![License](https://img.shields.io/badge/License-Apache-blue)
+
+## Gihub Username
+
+<strong><em>[Github](https://github.com/slugovoy)</em></strong>
+
+## Email Contact
+
+anandlal@yahoo.com
+
+
+    
--- index.js
+++ index.js
@@ -1,16 +1,67 @@
 // array of questions for user
-const questions = [
+const questions = require("./questions");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-];
+
+inquirer.prompt(questions).then((response) => {
+
+    // JSON.stringify(response)
+    fs.writeFile("README.md", `## Title
+
+${response.title}
+
+## Description
+
+${response.description}
+
+## Installation
+
+\`\`\`${response.installation}\`\`\`
+
+## Usage
+
+${response.usage}
+
+## Contribution
+
+${response.contribution}
+
+## Project Testing
+
+${response.test}
+
+## License
+
+![License](https://img.shields.io/badge/License-${response.license}-blue)
+
+## Gihub Username
+
+<strong><em>[Github](https://github.com/${response.github})</em></strong>
+
+## Email Contact
+
+${response.email}
+
+
+    `,(err) =>
+    err ? console.log(err) : console.log("Success!") ) 
+    })
+
+
+
+    
+
+
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 
-// function to initialize program
-function init() {
+// // function to initialize program
+// function init() {
 
-}
+// }
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
